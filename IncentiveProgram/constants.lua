28ef3d32efec100efc9e7bd1ebff37bbe0030f70
Euphorazine <Euphorazine@jacobbeu.com>
2022-12-10 12:58:17 -0600

    redid file structure for easier editing and adding new features in the future added toast options made role searching a setting instead of tied to current LFG Roles added ability to hold shift to see all instances so you can adjust settings when it is not a shortage

---
+++ IncentiveProgram/constants.lua
@@ -0,0 +1,118 @@
+-----------------------------
+------Incentive Program------
+----Created by: Jacob Beu----
+-----Xubera @ US-Alleria-----
+--------r5 | 10/24/2016------
+-----------------------------
+
+local addonName, IncentiveProgram = ...
+
+--Core
+IncentiveProgram.VERSION_NUMBER = 5
+IncentiveProgram.ADDON_DISPLAY_NAME = addonName.." (|cFF69CCF0r"..IncentiveProgram.VERSION_NUMBER.."|r)"
+
+IncentiveProgram.Flair = {
+    [849] = "HM1 - ",
+    [850] = "HM2 - ",
+    [851] = "HM3 - ",
+    [847] = "BRF1 - ",
+    [846] = "BRF2 - ",
+    [848] = "BRF3 - ",
+    [823] = "BRF4 - ",
+    [982] = "HC1 - ",
+    [983] = "HC2 - ",
+    [984] = "HC3 - ",
+    [985] = "HC4 - ",
+    [986] = "HC5 - ",
+    [1287] = "EN1 - ",
+    [1288] = "EN2 - ",
+    [1289] = "EN3 - ",
+    [1290] = "NH1 - ",
+    [1291] = "NH2 - ",
+    [1292] = "NH3 - ",
+    [1293] = "NH4 - "
+    
+}
+
+--Icon File Paths
+IncentiveProgram.Icons = {
+    ["INCENTIVE_NONE"] = "Interface\\ICONS\\Ability_Malkorok_BlightofYshaarj_Red",
+    ["INCENTIVE_RARE"] = "Interface\\Icons\\INV_Misc_Coin_17",
+    ["INCENTIVE_UNCOMMON"] = "Interface\\Icons\\INV_Misc_Coin_18",
+    ["INCENTIVE_PLENTIFUL"] = "Interface\\Icons\\INV_Misc_Coin_19",
+    ----------------------
+    ["CONTEXT_MENU_DIVIDER"] = "Interface\\Common\\UI-TooltipDivider-Transparent",
+    ["CONTEXT_MENU_RED_X"] = "Interface\\Common\\VOICECHAT-MUTED"
+  }
+  
+--Settings
+IncentiveProgram.Settings = {
+    QA_TANK = "queueAsTank",
+    QA_HEALER = "queueAsHealer",
+    QA_DAMAGE = "queueAsDamage",
+    IGNORE = "ignore",
+    DUNGEON_NAME = "dungeonName",
+    DUNGEON_TYPE = "dungeonType",
+    HIDE_IN_PARTY = "hideInParty",
+    HIDE_ALWAYS = "hideAlways", --still shows in databroker
+    ALERT = "alert",
+    ALERT_TOAST = "toastAlert",
+    COUNT_EVEN_IF_NOT_SELECTED = "countEvenIfNotSelected",
+    COUNT_EVEN_IF_NOT_ROLE_ELIGIBLE = "countEvenIfNotRoleEligible",
+    
+    ROLE_TANK = "roleTank",
+    ROLE_HEALER = "roleHealer",
+    ROLE_DAMAGE = "roleDamage",
+    
+    FRAME_TOP = "frameTop",
+    FRAME_LEFT = "frameLeft",
+    TOAST_TOP = "toastTop",
+    TOAST_LEFT = "toastLeft"
+}
+
+IncentiveProgram.TickRate = 20
+
+--Dungeon Constants
+IncentiveProgram.DUNGEON_REMOVED = 1
+IncentiveProgram.DUNGEON_ADDED = 2
+IncentiveProgram.DUNGEON_DIFFERENCE = 3
+
+IncentiveProgram.TOAST_TANK = "\124TInterface\\LFGFRAME\\UI-LFG-ICON-PORTRAITROLES:20:20:0:0:64:64:0:19:22:41\124t Tank"
+IncentiveProgram.TOAST_HEALER = "\124TInterface\\LFGFRAME\\UI-LFG-ICON-PORTRAITROLES:20:20:0:0:64:64:20:39:1:20\124t Healer"
+IncentiveProgram.TOAST_DAMAGE = "\124TInterface\\LFGFRAME\\UI-LFG-ICON-PORTRAITROLES:20:20:0:0:64:64:20:39:22:41\124t Damage"
+
+
+--Context Menu
+IncentiveProgram.ContextMenu = {
+    TANK = 2,
+    HEALER = 3,
+    DAMAGE = 4,
+    
+    ROLES = "roles",
+    IGNORE = "ignore",
+    SETTINGS = "settings",
+    
+    QUEUE = "queue",
+    JOIN = "join"
+}
+
+IncentiveProgram.ContextLabels = {
+    ROLES = "Roles",
+    TANK = "Tank",
+    HEALER = "Healer",
+    DAMAGE = "Damage",
+    
+    IGNORED = "Ignored",
+    NO_IGNORED = "No Ignored Dungeons",
+    
+    SETTINGS = "Settings",
+    HIDE_IN_PARTY = "Hide in Party",
+    HIDE_ALWAYS = "Hide Always (Data Brokers)",
+    ALERT = "Alert When New",
+    ALERT_TOAST = "Alert With Toast",
+    
+    IGNORE = "Ignore",
+    UNIGNORE = "Unignore",
+    
+    JOIN_QUEUE = "Join Queue"
+}
---
+++ IncentiveProgram/core.lua
@@ -0,0 +1,153 @@
+-----------------------------
+------Incentive Program------
+----Created by: Jacob Beu----
+-----Xubera @ US-Alleria-----
+--------r5 | 10/24/2016------
+-----------------------------
+
+local addonName, IncentiveProgram = ...
+
+local eventFrame = CreateFrame("Frame", "IncentiveProgramEventFrame", UIParent)
+eventFrame:RegisterEvent("VARIABLES_LOADED")
+eventFrame:SetScript("OnEvent", function(self, ...) self:OnEvent(...) end)
+eventFrame:SetScript("OnUpdate", function(self, ...) self:OnUpdate(...) end)
+
+-----------------------------------------
+---- Variables
+-----------------------------------------
+IncentiveProgram.SavedLFGRoles = {
+    isUpdated = false,
+    Leader = false,
+    Tank = false,
+    Healer = false,
+    Damage = false
+}
+
+-----------------------------------------
+---- Slash Command
+-----------------------------------------
+
+SLASH_INCENTIVEPROGRAM1 = "/ip"
+function SlashCmdList.INCENTIVEPROGRAM(msg, editbox)
+    IncentiveProgram:GetSettings():SetSetting(IncentiveProgram.Settings["HIDE_IN_PARTY"], false)
+    IncentiveProgram:GetSettings():SetSetting(IncentiveProgram.Settings["HIDE_ALWAYS"], false)
+    IncentiveProgram:GetFrame():ShowFrame()
+end
+
+-----------------------------------------
+---- OnEvent is the Event Handler for the Frame
+---- Registered Events:
+----      VARIABLES_LOADED
+----      GROUP_ROSTER_UPDATE
+----      LFG_UPDATE_RANDOM_INFO
+----      LFG_ROLE_UPDATE
+----      LFG_UPDATE
+-----------------------------------------
+function eventFrame:OnEvent(event, ...)
+    if ( event == "VARIABLES_LOADED" ) then
+        IncentiveProgram:SetCount(0)
+        IncentiveProgram:GetFrame():UpdatedSettings()
+        
+        self:RegisterEvent("GROUP_ROSTER_UPDATE")
+        self:RegisterEvent("LFG_UPDATE_RANDOM_INFO")
+        self:RegisterEvent("LFG_ROLE_UPDATE")
+        self:RegisterEvent("LFG_UPDATE")
+    elseif ( event == "GROUP_ROSTER_UPDATE" or event == "LFG_UPDATE" ) then --Party Update
+        if IsInGroup() then
+            if ( IncentiveProgram:GetSettings():GetSetting(IncentiveProgram.Settings["HIDE_IN_PARTY"]) ) then
+                IncentiveProgram:GetFrame():HideFrame()
+            end
+            
+            IncentiveProgram:SetCount(0)
+        else
+            IncentiveProgram:GetFrame():UpdatedSettings()
+            RequestLFDPlayerLockInfo()
+        end
+    elseif ( event == "LFG_UPDATE_RANDOM_INFO" ) then --Received new LFD Info
+        IncentiveProgram:SetCount(IncentiveProgram:GetDungeon():GetShortageCount())
+    elseif ( event == "LFG_ROLE_UPDATE" ) then --updated the role
+    
+    end
+    
+    if ( IncentiveProgram.SavedLFGRoles.isUpdated ) then
+        SetLFGRoles(IncentiveProgram.SavedLFGRoles.Leader, IncentiveProgram.SavedLFGRoles.Tank, IncentiveProgram.SavedLFGRoles.Healer, IncentiveProgram.SavedLFGRoles.Damage)
+        IncentiveProgram.SavedLFGRoles.isUpdated = false
+    end
+end
+
+function eventFrame:OnUpdate(e)
+    self.elapsed = self.elapsed or (IncentiveProgram.TickRate - 5)
+    self.elapsed = self.elapsed + e
+    self.alertCount = self.alertCount or 0
+    
+    if ( ( self.alertCount > 0 ) and ( self.elapsed > 1.5 ) ) then
+        if ( ( self.alertCount % 3 ) == 0 ) then
+            IncentiveProgram:SetCount(IncentiveProgram:GetDungeon():GetShortageCount()
+                ,IncentiveProgram.Icons["INCENTIVE_PLENTIFUL"])
+        elseif ( ( self.alertCount % 3 ) == 1 ) then
+            IncentiveProgram:SetCount(IncentiveProgram:GetDungeon():GetShortageCount()
+                ,IncentiveProgram.Icons["INCENTIVE_UNCOMMON"])
+        else
+            IncentiveProgram:SetCount(IncentiveProgram:GetDungeon():GetShortageCount()
+                ,IncentiveProgram.Icons["INCENTIVE_RARE"])
+        end
+        PlaySound("UI_GroupFinderReceiveApplication")
+        self.alertCount = self.alertCount - 1
+        
+        if ( self.alertCount > 0 ) then
+            self.elapsed = 0
+        else
+            self.elapsed = IncentiveProgram.TickRate - self.elapsed
+        end
+    end
+    
+    if self.elapsed > IncentiveProgram.TickRate then
+        self.elapsed = self.elapsed - IncentiveProgram.TickRate
+        RequestLFDPlayerLockInfo()
+    end
+end
+
+---------------------------------------
+-- SetCount is a global that can allow the setting of the count.  Updates frame and dbroker
+-- @params
+--      count - (Optional) Sets the text in the frame and the broker
+--		texture - (Optional) Sets the texture of the frame and the broker
+-- @returns
+--		nil
+---------------------------------------
+function IncentiveProgram:SetCount(count, texture)
+	if ( not count ) then count = 0 end
+    if ( not texture ) then
+        texture = IncentiveProgram.Icons["INCENTIVE_RARE"]
+    end
+
+    if ( count > 0 ) then
+        IncentiveProgram:GetFrame():ShowTextures(count, texture)
+        IncentiveProgram:GetDataBroker():SetData(count, texture)
+    else
+        IncentiveProgram:GetFrame():HideTextures()
+        IncentiveProgram:GetDataBroker():SetData(count, IncentiveProgram.Icons["INCENTIVE_NONE"])
+    end       
+end
+
+---------------------------------------
+-- SetAlert has the frame and broker chime or provide parameters for the Toast.
+-- @params
+--      line1 - (Optional) Line 1 of the Toast
+--		line2 - (Optional) Line 2 of the Toast
+--		texture - (Optional) texture of the Toast
+--		arg1 - (Optional) the dungeon ID of the Toast for the OnClick function
+--		arg2 - Not in use
+-- @returns
+--		nil
+---------------------------------------
+function IncentiveProgram:SetAlert(line1, line2, texture, arg1, arg2)
+    if ( IncentiveProgram:GetSettings():GetSetting(IncentiveProgram.Settings["ALERT"]) ) then
+        eventFrame.elapsed = 0 --reset timer for alert animation instead
+        eventFrame.alertCount = 6 -- how many times to cycle alert
+    end
+    
+    if ( IncentiveProgram:GetSettings():GetSetting(IncentiveProgram.Settings["ALERT_TOAST"]) ) then
+        IncentiveProgram:GetToast():AddToast(line1, line2, texture, arg1, arg2, IncentiveProgram:GetMenu().JoinDungeon)
+    end
+end
